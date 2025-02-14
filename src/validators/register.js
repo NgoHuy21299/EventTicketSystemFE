7d99783187b9639f91c6e6c983c5c4d3194b585c
Huy <quanghuy21299@gmail.com>
2025-02-14 15:48:16 +0700

    update register and row in transaction history

--- src/validators/register.js
+++ src/validators/register.js
@@ -4,7 +4,6 @@ import msg from "./message";
 
 const registerSchema = yup.object({
   fullName: yup.string().required(msg.required).matches(pattern.fullName, msg.fullName),
-  username: yup.string().required(msg.required).matches(pattern.username, msg.username),
   email: yup.string().required(msg.required).email(msg.email),
   phoneNumber: yup.string().required(msg.required).matches(pattern.phoneNumber, msg.phoneNumber),
   password: yup.string().required(msg.required).matches(pattern.password, msg.password),
