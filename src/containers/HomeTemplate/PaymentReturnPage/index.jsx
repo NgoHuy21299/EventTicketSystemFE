77e8b04c5950519336e06685d358ba67a0a1243e
SonNguyen <nguyenlamson1609@gmail.com>
2025-02-23 13:42:58 +0700

    Update css for payment page

--- src/containers/HomeTemplate/PaymentReturnPage/index.jsx
+++ src/containers/HomeTemplate/PaymentReturnPage/index.jsx
@@ -7,18 +7,15 @@ import cancelLogo from "@/assets/images/cancel.png";
 import Loader from "@/components/Loader";
 
 const handleBack = () => {
-  window.location.href = '/';
+  window.location.href = "/";
 };
-
-
-
-  const PaymentStatus = () => {
+const PaymentStatus = () => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.processPayment.data);
   const loading = useSelector((state) => state.processPayment.loading);
 
   useEffect(() => {
-     dispatch(actPaymentReturn());
+    dispatch(actPaymentReturn());
   }, [dispatch]);
 
   const renderLoader = () => {
@@ -26,44 +23,57 @@ const handleBack = () => {
   };
   return (
     <>
-    {renderLoader()}
-    {data && (
-    <div className="container">
-      {data?.responseCode === "00" ? (
-        <>
-          <img src={checkLogo} alt="Success" className="success-image" />
-          <div className="payment-success">
-            <h1>Thanh Toán Thành Công</h1>
-            <div className="transaction-info">
-              <h4>THÔNG TIN GIAO DỊCH</h4>
-              <p><strong>Hình thức thanh toán:</strong> {data?.bankCode}</p>
-              <p><strong>Nội dung:</strong> thanh toán cho đơn hàng {data?.bookingId}</p>
-              <p><strong>Số tiền thanh toán:</strong> {data?.amount} VND</p>
+      {renderLoader()}
+      {data && (
+        <div className="payment-page">
+          <div className="container">
+            {data?.responseCode === "00" ? (
+              <>
+                <img src={checkLogo} alt="Success" className="success-image" />
+                <div className="payment-success">
+                  <h1>Thanh Toán Thành Công</h1>
+                  <div className="transaction-info">
+                    <h4>THÔNG TIN GIAO DỊCH</h4>
+                    <p>
+                      <strong>Hình thức thanh toán:</strong> {data?.bankCode}
+                    </p>
+                    <p>
+                      <strong>Nội dung:</strong> thanh toán cho đơn hàng {data?.bookingId}
+                    </p>
+                    <p>
+                      <strong>Số tiền thanh toán:</strong> {data?.amount} VND
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <img src={cancelLogo} alt="Failed" className="success-image" />
+                <div className="payment-fail">
+                  <h1>Thanh Toán Thất Bại</h1>
+                  <div className="transaction-info">
+                    <h4>LỖI THANH TOÁN</h4>
+                    <p>
+                      <strong>Mã lỗi:</strong> {data?.responseCode}
+                    </p>
+                    <p>
+                      <strong>Vui lòng thử lại hoặc liên hệ hỗ trợ.</strong>
+                    </p>
+                  </div>
+                </div>
+              </>
+            )}
+
+            <div className="button-container">
+              <button onClick={handleBack} className="back-button">
+                Quay lại
+              </button>
             </div>
           </div>
-        </>
-      ) : (
-        <>
-          <img src={cancelLogo} alt="Failed" className="success-image" />
-          <div className="payment-fail">
-            <h1>Thanh Toán Thất Bại</h1>
-            <div className="transaction-info">
-              <h4>LỖI THANH TOÁN</h4>
-              <p><strong>Mã lỗi:</strong> {data?.responseCode}</p>
-              <p><strong>Vui lòng thử lại hoặc liên hệ hỗ trợ.</strong></p>
-            </div>
-          </div>
-        </>
+        </div>
       )}
-      
-      <div className="button-container">
-        <button onClick={handleBack} className="back-button">Quay lại</button>
-      </div>
-    </div>
-    )}
     </>
   );
-
 };
 
 export default PaymentStatus;
